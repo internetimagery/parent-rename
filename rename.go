053be9e3c3018df3133d187720ed42aad6ded1a9
Jason Dixon <jason.dixon.email@gmail.com>
2017-03-24 01:43:33 +1300

    directories listed!

--- rename.go
+++ rename.go
@@ -7,7 +7,22 @@ import (
   "fmt"
   "os"
   "path/filepath"
+  "io/ioutil"
 )
+
+// Directory prepped for renaming
+type Directory struct {
+  Name string
+  Files []os.FileInfo
+}
+
+// Directory Factory
+func NewDirectory(path string) (*Directory) {
+  dir := new(Directory)
+  dir.Name = filepath.Base(path)
+  dir.Files = getfiles(path)
+  return dir
+}
 
 // Check if path is a directory
 func isdir(path string) bool {
@@ -17,18 +32,35 @@ func isdir(path string) bool {
   return false
 }
 
+// Get all files from a directory
+func getfiles(path string) ([]os.FileInfo) {
+  if paths, err := ioutil.ReadDir(path); err == nil {
+    result := make([]os.FileInfo, 0)
+    for _, p := range paths {
+      if !p.IsDir() {
+        result = append(result, p)
+      }
+    }
+    return result
+  } else {
+    panic(err)
+  }
+}
+
 func main()  {
   // Grab our cli arguments
   args := os.Args[1:]
   if len(args) > 0 {
     // Get our current dir
     cwd, _ := os.Getwd()
+    // Store valid directories. Use map to eliminate doubleups
+    dirs := make(map[string]*Directory)
     for _, p := range args {
-      abs := filepath.Join(cwd, p)
-      if isdir(abs) {
-        fmt.Println("OK", abs)
+      if abs := filepath.Join(cwd, p); isdir(abs) {
+        dirs[abs] = NewDirectory(abs)
       }
     }
+    fmt.Println("Directories", dirs)
   } else {
     // No args, print help menu
     fmt.Println("Usage: rename <directory> ...")
