9e48d9b4880230032d50fd7d8b41bde6dbc7a979
Jason Dixon <jason.dixon.email@gmail.com>
2017-03-24 00:02:44 +1300

    adding main file

---
+++ main.go
@@ -0,0 +1,12 @@
+// Rename files to that of their containing folder.
+// Also ignore anything in square brackets, for
+// compatability with TagSpaces.
+package main
+
+import (
+  "fmt"
+)
+
+func main()  {
+  fmt.Println("Hello world!")
+}
